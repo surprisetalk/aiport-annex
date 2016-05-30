7cb7130d6950be01725f6df1d5a5e17baea179d9
Taylor Sarrafian <surprisetalk@gmail.com>
2016-05-30 05:18:43 +0000

    🐒

--- annex.js
+++ annex.js
@@ -2,7 +2,13 @@
 var package = require('aiport-package');
 
 // TODO: pile injection?
-// TODO: errors
 // TODO: require('aiport-dev').annex({...}) should intelligently interpret index
 // KLUDGE
-module.exports = annex_name => require( "aiport-annex-" + package.installed().annex[ annex_name ] ).index;
+module.exports = annex_name => 
+{
+    var annexes = package.installed().annex;
+    return annex_name in annexes 
+	? Promise.resolve( require( "aiport-annex-" + annexes[ annex_name ] ).index )
+	: Promise.reject( { code: 404, msg: "could not find annex named " + annex_name } );
+};
+
