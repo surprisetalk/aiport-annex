9eb5c7b4d9b81db2e1f2afc41132002a777c5607
Taylor Sarrafian <surprisetalk@gmail.com>
2016-05-31 02:00:01 +0000

    😬

--- annex.js
+++ annex.js
@@ -1,14 +1,20 @@
 
-var package = require('aiport-package');
+var path = require('path');
+var _ = require('underscore');
+var package = require('../aiport-package/package.js');
 
 // TODO: pile injection?
 // TODO: require('aiport-dev').annex({...}) should intelligently interpret index
 // KLUDGE
-module.exports = annex_name => 
+module.exports = ( annex_name, params, query ) => 
 {
+    var root = path.dirname( require.main.filename ) + "/node_modules";
     var annexes = package.installed().annex;
-    return annex_name in annexes 
-	? Promise.resolve( require( "aiport-annex-" + annexes[ annex_name ] ).index )
+    
+    return _.contains( annexes, annex_name )
+	// TODO: check if exists
+	? require('../aiport-annex-package/package.js')( params, query )
+	// ? require( root + "/aiport-annex-" + annex_name )( params, query )
 	: Promise.reject( { code: 404, msg: "could not find annex named " + annex_name } );
 };
 
